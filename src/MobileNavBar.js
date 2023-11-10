60adb0d80d91f3cbb8c7fdfa6e0a9a2e50f9a944
Ahmed khan <ahmed_khan019@hotmail.com>
2023-11-10 21:29:54 +0500

    Added Mobile navbar

---
+++ src/MobileNavBar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Disclosure } from '@headlessui/react'
+
+export default function MobileNavBar(navigation, classNames) {
+  return (
+    <Disclosure.Panel className="sm:hidden">
+        <div className="space-y-1 px-2 pb-3 pt-2">
+            {navigation.map((item) => (
+                <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'block rounded-md px-3 py-2 text-base font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                >
+                    {item.name}
+                </Disclosure.Button>
+            ))}
+        </div>
+    </Disclosure.Panel>
+  )
+}
