a857c45b1a3d2a6284d3ca141bb82cfec206328d
Ahmed khan <ahmed_khan019@hotmail.com>
2023-11-13 23:26:35 +0500

    Changed classes to be generic

--- src/Cards.js
+++ src/Cards.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Cards({heading}) {
   return (
-    <div className='total-users-container'>
+    <div className='total-container'>
         <h1>{heading}:</h1>
-        <div className='toal-users-charts'></div>
+        <div className='toal-charts'></div>
     </div>
   )
 }
