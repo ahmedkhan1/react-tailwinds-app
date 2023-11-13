b2a7cd9e5e5d646211a63f3418e397ed2eb4706e
Ahmed khan <ahmed_khan019@hotmail.com>
2023-11-13 23:26:05 +0500

    Added headings props to cards component

--- src/Cards.js
+++ src/Cards.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Cards() {
+export default function Cards({heading}) {
   return (
     <div className='total-users-container'>
-        <h1>Total Users:</h1>
+        <h1>{heading}:</h1>
         <div className='toal-users-charts'></div>
     </div>
   )
