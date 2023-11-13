34900cd4072e4c6afb09514e4e506893ac5d8e89
Ahmed khan <ahmed_khan019@hotmail.com>
2023-11-13 23:25:36 +0500

    Added Cards component structure

--- src/Cards.js
+++ src/Cards.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Cards() {
   return (
-    <div>Cards</div>
+    <div className='total-users-container'>
+        <h1>Total Users:</h1>
+        <div className='toal-users-charts'></div>
+    </div>
   )
 }
