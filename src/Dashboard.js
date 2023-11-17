bc97e70ff82f8fbd27936ada35be713db5ed43e1
Ahmed khan <ahmed_khan019@hotmail.com>
2023-11-17 23:24:31 +0500

    Added users heading

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Cards from './Cards'
 
 export default function Dashboard({heading}) {
-  const revenue = "Total Users";
-  return (
-    <>
-        <h1 className='dashboard-heading'>{heading}</h1>
-        <hr />
-        <Cards heading={"Total Users"} />
-        <Cards heading={revenue} />
-    </>
-  )
+    const revenue = "Total revenue";
+    const users = "Total Users";
+    return (
+        <>
+            <h1 className='dashboard-heading'>{heading}</h1>
+            <hr />
+            <Cards heading={users} />
+            <Cards heading={revenue} />
+        </>
+    )
 }
