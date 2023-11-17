b4858171ab638e27d28ebbbcc7c2a91f9e7a4397
Ahmed khan <ahmed_khan019@hotmail.com>
2023-11-17 23:23:43 +0500

    Added revenue heading

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Cards from './Cards'
 
 export default function Dashboard({heading}) {
+  const revenue = "Total Users";
   return (
     <>
         <h1 className='dashboard-heading'>{heading}</h1>
         <hr />
         <Cards heading={"Total Users"} />
-        <Cards heading={"Total revenue"} />
+        <Cards heading={revenue} />
     </>
   )
 }
