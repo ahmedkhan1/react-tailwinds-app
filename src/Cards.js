89f2e408bccd2f6cae25d75ad5be8b45792bb347
Ahmed khan <ahmed_khan019@hotmail.com>
2023-11-18 23:55:48 +0500

    Added headng conditionally for cards

--- src/Cards.js
+++ src/Cards.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Cards({heading}) {
   return (
     <div className='total-container'>
+      {
+        heading &&
         <h1>{heading}:</h1>
+      }
         <div className='toal-charts'></div>
     </div>
   )
